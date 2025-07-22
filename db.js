5a3c260e8471b0b05dc0fc946240fb500487f987
Elvia-99 <42182736+Elvia-99@users.noreply.github.com>
2025-07-22 16:28:29 -0500

    Add files via upload

--- db.js
+++ db.js
@@ -1,6 +1,14 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-app.js";
 import { getFirestore, doc, setDoc } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-firestore.js";
-import { firebaseConfig } from "./firebase.js";
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyAw6p2srtCBwJk3RHz4hkQ5GG9dV_ZFFuc",
+  authDomain: "saludpredictai.firebaseapp.com",
+  projectId: "saludpredictai",
+  storageBucket: "saludpredictai.firebasestorage.app",
+  messagingSenderId: "706368280280",
+  appId: "1:706368280280:web:d3417482e15641ec9629ad"
+};
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
