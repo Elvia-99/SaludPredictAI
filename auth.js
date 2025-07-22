0476bc58d17573e97f8edf30a2793700e05d520c
Elvia-99 <42182736+Elvia-99@users.noreply.github.com>
2025-07-22 16:12:14 -0500

    Update auth.js

--- auth.js
+++ auth.js
@@ -4,9 +4,16 @@ import {
   signInWithEmailAndPassword,
   signInWithPopup
 } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-auth.js";
+import { guardarUsuario } from './db.js';
 
-export function register(email, password) {
-  return createUserWithEmailAndPassword(auth, email, password);
+export function register(email, password, tipo = "paciente") {
+  return createUserWithEmailAndPassword(auth, email, password).then(cred => {
+    return guardarUsuario(cred.user.uid, {
+      correo: email,
+      tipo: tipo,
+      creadoEn: new Date()
+    });
+  });
 }
 
 export function login(email, password) {
